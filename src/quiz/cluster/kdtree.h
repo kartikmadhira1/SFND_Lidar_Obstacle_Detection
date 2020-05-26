841d25f731edd756eea527b87b814fc69b94a4bd
Kartik Madhira <kartikmadhira1@gmail.com>
2020-05-26 12:29:52 -0400

    kd tree insert implementation

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -29,8 +29,29 @@ struct KdTree
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
-
+		
 	}
+
+	void insertHelper(std::vector<float> point, Node **node, int depth, int id) {
+		if (*node == NULL) {
+			*node = new Node(point, id);
+		} else {
+			if (depth%2 == 0) {
+				if (point[0] < (*node)->point[0]) {
+					insertHelper(point, &(*node)->left, depth+1, id);
+				} else {
+					insertHelper(point, &(*node)->right, depth+1, id);
+				}
+			} else {
+				if (point[1] < (*node)->point[1]) {
+					insertHelper(point, &(*node)->left, depth+1, id);
+				} else {
+					insertHelper(point, &(*node)->right, depth+1, id);
+				}
+			}
+		}
+	}
+
 
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
